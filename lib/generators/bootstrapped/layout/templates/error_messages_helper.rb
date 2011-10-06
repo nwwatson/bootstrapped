ccc0dad652c75bc1a8e3edc2ef45380a4ae4aeb7
Russell Jennings <russ@geekwhiz.com>
2011-10-06 02:59:25 -0400

    changed error_messages_for class to reflect bootstrap styling

--- lib/generators/bootstrapped/layout/templates/error_messages_helper.rb
+++ lib/generators/bootstrapped/layout/templates/error_messages_helper.rb
@@ -6,7 +6,8 @@ module ErrorMessagesHelper
     options[:message] ||= I18n.t(:"activerecord.errors.message", :default => "Correct the following errors and try again.")
     messages = objects.compact.map { |o| o.errors.full_messages }.flatten
     unless messages.empty?
-      content_tag(:div, :class => "error_messages") do
+        # forced as error but may not abstractly always be the case
+      content_tag(:div, :class => "alert-message block-message error") do
         list_items = messages.map { |msg| content_tag(:li, msg.html_safe) }
         content_tag(:h2, options[:header_message].html_safe) + content_tag(:p, options[:message].html_safe) + content_tag(:ul, list_items.join.html_safe)
       end
