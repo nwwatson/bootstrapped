975db0225f36bab85964d6014a7fea022d895636
Nicholas Watson <nwwatson@gmail.com>
2012-02-08 08:22:35 -0500

    removed temp files

--- tmp/rails_app/app/models/project.rb
+++
@@ -1,3 +0,0 @@
-class Project < ActiveRecord::Base
-  attr_accessible :name
-end
--- tmp/rails_app/test/unit/project_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProjectTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Project.new.valid?
-  end
-end
