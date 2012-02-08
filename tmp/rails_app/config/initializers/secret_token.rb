a77d9ca54a97f0eb25f217204f1221e7ed44d262
Nicholas Watson <nwwatson@gmail.com>
2012-02-08 08:21:53 -0500

    adding cucumber test for installer

---
+++ tmp/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '342fcc9d010e080e61865e0613465575cbb02e962bdb406b6e3e1dbc578c9dca867907204f8de8600cb1aadc821ed7a60781c759ddbfb744284fb30d48c336ad'
