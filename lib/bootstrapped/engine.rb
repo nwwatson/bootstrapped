0360eabd85cb3dd37b4c78c422b0e4dac8cdc1b1
Nicholas Watson <nwwatson@gmail.com>
2012-03-06 08:15:31 -0500

    updated versions

--- lib/bootstrapped/engine.rb
+++ lib/bootstrapped/engine.rb
@@ -2,18 +2,12 @@ require 'rails'
 
 module Bootstrapped
   class Engine < ::Rails::Engine
-    unless RUBY_PLATFORM.eql?('java')
-      initializer 'bootstrapped.setup', 
-        :after => 'less-rails.after.load_config_initializers', 
-        :group => :all do |app|
-          app.config.less.paths << File.join(config.root, 'vendor', 'less')
-      end
+
+    initializer 'twitter-bootstrap-rails.setup', 
+      :after => 'less-rails.after.load_config_initializers', 
+      :group => :all do |app|
+        app.config.less.paths << File.join(config.root, 'vendor', 'less',)
     end
-    
-    #initializer 'bootstrapped-less.setup', :after => 'less-rails.after.load_config_initializers', :group => :all do |app|
-    #  app.config.less.paths << File.join(app.root, 'app', 'assets', 'stylesheets','bootstrap','less')
-    #end
-    
     
   end
 end
